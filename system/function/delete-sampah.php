e0f0a28eb608cfba222b1291804ccf644b46dd3d
ptr-21 <157449192+ptr-21@users.noreply.github.com>
2024-06-07 17:28:48 +0700

    Update delete-sampah.php

--- system/function/delete-sampah.php
+++ system/function/delete-sampah.php
@@ -1,5 +1,5 @@
 <?php
- require_once("E:/xampp/htdocs/Eco-Cash/system/config/koneksi.php");
+ require_once("http://localhost/Eco-Cash/system/config/koneksi.php");
  $id = $_GET['id'];
  $query = "DELETE FROM sampah WHERE jenis_sampah = '$id'";
  $queryact = mysqli_query($conn, $query);
