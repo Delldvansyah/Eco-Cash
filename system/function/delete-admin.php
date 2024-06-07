c30f5061c5f53f4ca41ec3badfe86b26e72f2940
ptr-21 <157449192+ptr-21@users.noreply.github.com>
2024-06-07 17:28:25 +0700

    Update delete-admin.php

--- system/function/delete-admin.php
+++ system/function/delete-admin.php
@@ -1,5 +1,5 @@
 <?php
- require_once("E:/xampp/htdocs/Eco-Cash/system/config/koneksi.php");
+ require_once("http://localhost/Eco-Cash/system/config/koneksi.php");
  $id = $_GET['id'];
  $query = "DELETE FROM admin WHERE nia = '$id'";
  $queryact = mysqli_query($conn, $query);
