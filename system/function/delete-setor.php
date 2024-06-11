aaf1c8fca18e20989d4036d105e095a08450ecde
Dellon Davansyah <dellondavansyah929@gmail.com>
2024-06-11 23:03:44 +0700

    setor tarik

--- system/function/delete-setor.php
+++ system/function/delete-setor.php
@@ -1,8 +1,8 @@
 <?php
- require_once("http://localhost/Eco-Cash/system/config/koneksi.php");
+ require_once("E:/xampp/htdocs/Eco-cash/system/config/koneksi.php");
  $id = $_GET['id'];
  $query = "DELETE FROM setor WHERE id_setor = '$id'";
  $queryact = mysqli_query($conn, $query);
  echo "<meta http-equiv='refresh'
-              content='0; url=http://localhost/ecocash/page/admin.php?page=data-setor'>";
+            content='0; url=http://localhost/Eco-Cash/page/admin.php?page=data-setor'>";
 ?>
--- system/function/view-tarik.php
+++ system/function/view-tarik.php
@@ -2,25 +2,23 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Penarikan Saldo</title>
 	<link rel="stylesheet" type="text/css" href="http://localhost/Eco-Cash/datatables/css/jquery.dataTables.css">
 	<style>
 		label{
-        font-family: Montserrat;    
-        font-size: 18px;
-        display: block;
-        color: #262626;
-        }
-
+			font-family: Montserrat;    
+			font-size: 18px;
+			display: block;
+			color: #262627;
+		}
 	</style>
 </head>
 <body>
-	<h2 style="font-size: 30px; color: #262626;">Transaksi Tarik Saldo</h2>
+	<h2 style="font-size: 30px; color: #262626;">Penarikan Saldo</h2>
 	<br>
-	<table id="example" class="display" cellspacing="0" width="100%" border="0" >
+	<table id="example" class="display" cellspacing="0" width="100%" border="0">
         <thead>
         <tr>
-            <th>No</th>
             <th>ID</th>
             <th>Tanggal</th>
             <th>NIN</th>
@@ -32,7 +30,6 @@
         </thead>
         <tfoot>
         <tr>
-            <th>No</th>
             <th>ID</th>
             <th>Tanggal</th>
             <th>NIN</th>
@@ -44,12 +41,11 @@
         </tfoot>
         <tbody>
         <?php
-            $no = 0;
+            include 'E:/xampp/htdocs/Eco-Cash/system/config/koneksi.php';
             $query = mysqli_query($conn, "SELECT * FROM tarik ORDER BY id_tarik ASC");
-            while($row = mysqli_fetch_assoc($query)){$no++;
+            while($row = mysqli_fetch_assoc($query)){
         ?>
         <tr align="center">
-            <td><?php echo "$no" ?></td>
             <td><?php echo $row['id_tarik'] ?></td>
             <td><?php echo $row['tanggal_tarik'] ?></td>
             <td><?php echo $row['nin'] ?></td>
@@ -57,15 +53,12 @@
             <td><?php echo "Rp. ".number_format($row['jumlah_tarik'], 2, ",", ".")  ?></td>
             <td><?php echo $row['nia'] ?></td>
             <td>
-                
-                <a href="a">
+                <a href="admin.php?page=edit-tarik&id=<?php echo $row['id_tarik']; ?>">
                 <button><i class="fa fa-pencil"></i>edit</button> 
                 </a>
-
                 <a onclick="return confirm('Anda yakin ingin menghapus data ini?')" href="http://localhost/Eco-Cash/system/function/delete-tarik.php?id=<?php echo $row['id_tarik']; ?>">
                 <button><i class="fa fa-trash-o"></i>hapus</button>
                 </a>
-
             </td>
         </tr>
         <?php } ?>
@@ -73,27 +66,21 @@
     </table>
     <br>
     <br>
-    
     <a href="admin.php?page=tambah-data-tarik">
     <button><i class="fa fa-plus" aria-hidden="true"></i>Tambah</button>
     </a>
-
     <a target="_blank" href="http://localhost/Eco-Cash/system/function/excel-tarik.php">
     <button><i class="fa fa-print" aria-hidden="true"></i>Excel</button>
     </a>
-
     <a target="_blank" href="http://localhost/Eco-Cash/system/function/print-tarik.php">
     <button><i class="fa fa-print" aria-hidden="true"></i>Cetak</button>
     </a>
-    
-    <script type="text/javascript" src="http://localhost/Eco-Cash/datatables/js/jquery.min.js"></script>
-    <script type="text/javascript" src="http://localhost/Eco-Cash/datatables/js/jquery.dataTables.min.js"></script>
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.4/css/select2.min.css" rel="stylesheet" />
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.4/js/select2.min.js"></script>
+    <script type="text/javascript" src="http://localhost/Eco-Cash/datatables/js/jquery.js"></script>
+    <script type="text/javascript" src="http://localhost/Eco-Cash/datatables/js/jquery.dataTables.js"></script>
     <script>
-        $(document).ready(function() {
-           $('#example').DataTable();
-        } );
+    $(document).ready(function() {
+        $('#example').DataTable();
+    });
     </script>
 </body>
 </html>
